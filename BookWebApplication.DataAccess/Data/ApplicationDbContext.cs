d91af4ea000d616d7b30c74faaff2c495c9bd42d
Foryou <hjw7548@gmail.com>
2020-06-29 16:46:23 +0900

    Section 6 Clear! But I must learn more this section!

--- BookWebApplication.DataAccess/Data/ApplicationDbContext.cs
+++ BookWebApplication.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookWebApplication.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace BookWebApplication.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
